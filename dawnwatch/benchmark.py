882c61265577fee026dacf0a6cee3c79117d1de8
dansamuka <dansamuka@gmail.com>
2026-09-24 13:47:31 +0300

    Normalize P1A benchmark import order

--- dawnwatch/benchmark.py
+++ dawnwatch/benchmark.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from pydantic import BaseModel
 
 from dawnwatch.archive import ArchiveRepository
-from dawnwatch.history import STATE_RANK, HistoricalCase, replay_checkpoints
+from dawnwatch.history import HistoricalCase, STATE_RANK, replay_checkpoints
 from dawnwatch.models import RiskState
 
 
